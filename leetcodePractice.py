4515a0a75ab85bdd9df2ac67749dac9011f54b7c
kissmihalyit <124740303+kissmihaly1@users.noreply.github.com>
2023-04-18 00:16:47 +0200

    Update leetcodePractice.py

--- leetcodePractice.py
+++ leetcodePractice.py
@@ -1,8 +1,7 @@
-import string
 
 
 class day1:
-    def removeStars(s: string):
+    def removeStars(s: str):
         stack=[]
         for letter in s:
             if letter =='*':
@@ -11,7 +10,36 @@ class day1:
                 stack.append(letter)
         return ''.join(stack)
 
-    print(removeStars("kis**kacsa"))
+    #print(removeStars("kis**kacsa"))
 
 
+    def palindromeNumber9 (x: int) -> bool:
+        if x < 0:
+            return False
+        rev = 0
+        orig = x
+        while x != 0:
+            rev = rev * 10 + x % 10
+            x //= 10
+        return rev == orig
 
+    #print(palindromeNumber9(121))
+
+    def romantointeger13(s:str) ->int:
+            translations = {
+                "I": 1,
+                "V": 5,
+                "X": 10,
+                "L": 50,
+                "C": 100,
+                "D": 500,
+                "M": 1000
+            }
+            number = 0
+            s = s.replace("IV", "IIII").replace("IX", "VIIII")
+            s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+            s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+            for char in s:
+                number += translations[char]
+            return number
+    print(romantointeger13(s="XX"))
