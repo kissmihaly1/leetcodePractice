c9632b7c5538cb5f88768784943f5e6d9a39af70
kissmihalyit <124740303+kissmihaly1@users.noreply.github.com>
2023-04-19 19:12:29 +0200

    daily update

--- leetcodePractice.py
+++ leetcodePractice.py
@@ -1,6 +1,7 @@
+from typing import List
 
 
-class day1:
+class Day1:
     def removeStars(s: str):
         stack=[]
         for letter in s:
@@ -43,3 +44,34 @@ class day1:
                 number += translations[char]
             return number
     print(romantointeger13(s="XX"))
+
+
+class Day2:
+    #We have two special characters:
+    #The first character can be represented by one bit 0.
+    #The second character can be represented by two bits (10 or 11).
+    #Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+
+
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i = 0
+        while i < len(bits) - 1:
+            if bits[i] == 1:
+                i += 2
+            else:
+                i += 1
+        return i == len(bits) - 1
+
+
+class Day3:
+    #2574. Left and Right Sum Differences
+    def leftRigthDifference(self, nums: List[int]) -> List[int]:
+        ans=[0]
+        for i in nums: ans+=[ans[-1]+i]
+        a=[]
+        for i in range(1,len(ans)):
+            a+=[abs(ans[-1]-ans[i]-ans[i-1])]
+        return a
+
+
+
