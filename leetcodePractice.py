dbe7810ef6d1310672067ff01f39d606e54ae86c
kissmihalyit <124740303+kissmihaly1@users.noreply.github.com>
2023-04-20 21:14:54 +0200

    daily coding

--- leetcodePractice.py
+++ leetcodePractice.py
@@ -3,18 +3,17 @@ from typing import List
 
 class Day1:
     def removeStars(s: str):
-        stack=[]
+        stack = []
         for letter in s:
-            if letter =='*':
+            if letter == '*':
                 stack.pop()
             else:
                 stack.append(letter)
         return ''.join(stack)
 
-    #print(removeStars("kis**kacsa"))
+    # print(removeStars("kis**kacsa"))
 
-
-    def palindromeNumber9 (x: int) -> bool:
+    def palindromeNumber9(x: int) -> bool:
         if x < 0:
             return False
         rev = 0
@@ -24,33 +23,32 @@ class Day1:
             x //= 10
         return rev == orig
 
-    #print(palindromeNumber9(121))
+    # print(palindromeNumber9(121))
 
-    def romantointeger13(s:str) ->int:
-            translations = {
-                "I": 1,
-                "V": 5,
-                "X": 10,
-                "L": 50,
-                "C": 100,
-                "D": 500,
-                "M": 1000
-            }
-            number = 0
-            s = s.replace("IV", "IIII").replace("IX", "VIIII")
-            s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
-            s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
-            for char in s:
-                number += translations[char]
-            return number
+    def romantointeger13(s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
 
 
 class Day2:
-    #We have two special characters:
-    #The first character can be represented by one bit 0.
-    #The second character can be represented by two bits (10 or 11).
-    #Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
-
+    # We have two special characters:
+    # The first character can be represented by one bit 0.
+    # The second character can be represented by two bits (10 or 11).
+    # Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
 
     def isOneBitCharacter(self, bits: List[int]) -> bool:
         i = 0
@@ -63,25 +61,29 @@ class Day2:
 
 
 class Day3:
-    #2574. Left and Right Sum Differences
+    # 2574. Left and Right Sum Differences
     def leftRigthDifference(self, nums: List[int]) -> List[int]:
-        ans=[0]
-        for i in nums: ans+=[ans[-1]+i]
-        a=[]
-        for i in range(1,len(ans)):
-            a+=[abs(ans[-1]-ans[i]-ans[i-1])]
+        ans = [0]
+        for i in nums: ans += [ans[-1] + i]
+        a = []
+        for i in range(1, len(ans)):
+            a += [abs(ans[-1] - ans[i] - ans[i - 1])]
         return a
 
 
 class Day4:
-    #66. Plus One
+    # 66. Plus One
     def plusOne(digits: List[int]) -> List[int]:
         result = []
         number = 0
         val = 0
 
         if len(digits) == 1:
-            result = [digits[0] + 1]
+            if digits[0] == 9:
+                result.append(1)
+                result.append(0)
+            else:
+                result = [digits[0] + 1]
             return result
 
         for i in range(len(digits)):
@@ -93,4 +95,4 @@ class Day4:
 
         return result
 
-    print(plusOne(digits=[1, 2, 3, 4]))
+    print(plusOne(digits=[9]))
