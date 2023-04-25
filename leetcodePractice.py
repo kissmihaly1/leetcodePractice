687648d5b74a1027bfe59621e46be1bc809d5620
kissmihalyit <124740303+kissmihaly1@users.noreply.github.com>
2023-04-25 22:29:21 +0200

    daily coding

--- leetcodePractice.py
+++ leetcodePractice.py
@@ -149,4 +149,30 @@ class Day7_apr24:
 
         return "".join(result)
 
-    print(removeDuplicates(s="abbaca"))
+    #print(removeDuplicates(s="abbaca"))
+
+class Day8_apr25:
+    #860. Lemonade Change
+    def lemonadeChange(bills: List[int]) -> bool:
+        count5 = 0
+        count10 = 0
+        for b in bills:
+            if b == 5:
+                count5+=1
+            elif b == 10:
+                if count5 == 0:
+                    return False
+                count10+=1
+                count5-=1
+            else:
+                if (count5>=1 and count10>=1):
+                    count5-=1
+                    count10-=1
+                elif count5>=3:
+                    count5-=3
+                else:
+                    return False
+        return True
+    #print(lemonadeChange(bills=[5,5,10,10,20]))
+
+
